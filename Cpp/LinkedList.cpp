36d89a27abda922c524d7489e672e6db9992243e
Yarushi.ThanhTinh <thanhtinh.yarushi@gmail.com>
2023-10-04 12:02:15 +0700

    make Node

--- Cpp/LinkedList.cpp
+++ Cpp/LinkedList.cpp
@@ -1,7 +1,17 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-
+struct NODE{
+    int data;
+    struct NODE* next;
+};
+typedef struct NODE* Node;
+NODE* makeNode(int x){
+    NODE* newNode = (NODE*)malloc(sizeof(NODE));
+    newNode->data = x;
+    newNode->next = NULL;
+    return newNode;
+}
 
 
 int main(){
