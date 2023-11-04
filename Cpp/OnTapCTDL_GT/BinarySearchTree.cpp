9ee5dc81ff22eadc253153dbf4ac12bb9f850442
Yarushi.ThanhTinh <thanhtinh.yarushi@gmail.com>
2023-11-04 10:55:12 +0700

    add bst cpp file

---
+++ Cpp/OnTapCTDL_GT/BinarySearchTree.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+
+struct NODE{
+    int data;
+    NODE* pleft;
+    NODE* pright;
+};
+
+NODE* makeNode(int x){
+    NODE* newNode = new NODE();
+    newNode->data = x;
+    newNode->pleft = newNode->pright = NULL;
+    return newNode;
+}
+
+int main(){
+    return 0;
+}
