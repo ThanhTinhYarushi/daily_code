14bce9ccb3526047cb17b12efe7beb49b27b1cf0
Yarushi <thanhtinh.yarushi@gmail.com>
2023-12-02 18:44:00 +0700

    Khoi Tao & Huy

--- Cpp/git.cpp
+++ Cpp/git.cpp
@@ -5,12 +5,16 @@ class Person {
         std::string Name;
         int age;
     public:
-    Person();
-    ~Person();
-
+        Person();
+        ~Person();
 };
 
+Person::Person() { std::cout << "Khoi Tao" << std::endl; }
+
+Person::~Person() { std::cout << "Huy" << std::endl; }
+
 int main() {
-    std::cout << "Hello World" << std::endl;
+    Person Thanh_Tinh;
+
     return 0;
 }
