4ba5a6a62aa3d285881d78d1dffa3b3b34770249
ThanhTinhYarushi <thanhtinh.yarushi@gmail.com>
2024-01-03 17:26:52 +0700

    daily code flexbox basic

--- Cpp/git.cpp
+++ Cpp/git.cpp
@@ -5,16 +5,37 @@ class Person {
         std::string Name;
         int age;
     public:
-        Person();
+        Person(std::string Name,int Age);
         ~Person();
+        int getAge();
+        std::string getName();
+        void Hello();
 };
 
-Person::Person() { std::cout << "Khoi Tao" << std::endl; }
+// Person::Person() { std::cout << "Khoi Tao" << std::endl; }
+Person::Person(std::string Name,int Age) {
+    this->Name = Name;
+    this->age = Age;
+}
+
+std::string Person::getName() {
+    return this->Name;
+}
+
+int Person::getAge() {
+    return this->age;
+}
+
+
+void Person::Hello() {
+    std::cout << "Hello" << std::endl;
+}
 
 Person::~Person() { std::cout << "Huy" << std::endl; }
 
 int main() {
-    Person Thanh_Tinh;
-
+    Person Thanh_Tinh("Thanh Tinh",20);
+    Thanh_Tinh.Hello();
+    
     return 0;
 }
