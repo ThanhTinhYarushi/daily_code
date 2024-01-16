47facd30a73e0e750bbcb6aec8aa987a60ef578b
ThanhTinhYarushi <thanhtinh.yarushi@gmail.com>
2024-01-16 14:56:34 +0700

    js slider

--- HTML/GiaoDienSach_ThuyVy/main.js
+++ HTML/GiaoDienSach_ThuyVy/main.js
@@ -9,7 +9,7 @@ var idx = 1;
 var cssHeading = ".text-content .text-heading";
 var cssContent = ".text-content .text-description";
 setInterval(function () {
-  var slider = document.querySelector(".slider");
+  var slider = document.querySelector("#slider");
   document.querySelector(cssHeading).innerText = content[idx].split("|")[0];
   document.querySelector(cssContent).innerText = content[idx].split("|")[1];
   slider.style.background = `url('./assets/img/slider/${
