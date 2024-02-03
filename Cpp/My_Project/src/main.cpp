3a7a89f9735fbe1cd47c3f5ddb2fe17f6addd8b8
ThanhTinhYarushi <thanhtinh.yarushi@gmail.com>
2024-02-03 15:31:05 +0700

    Push

---
+++ Cpp/My_Project/src/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+void XinChao(void)
+{
+    for(int i=1; i<=10 ; i++) 
+    {
+        std::cout << i << ". Xin Chao Cac Ban nghe!!" << std::endl;
+    }
+}
+
+int main()
+{
+    XinChao();
+    return 0;
+} 
