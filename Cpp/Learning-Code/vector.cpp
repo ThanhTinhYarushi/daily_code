5d2ebf202b332bfd2cfd7d6591f7b77da8a3c01b
ThanhTinhYarushi <thanhtinh.yarushi@gmail.com>
2024-02-15 15:31:04 +0700

    Nhap Xuat Co ban

---
+++ Cpp/Learning-Code/vector.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+
+const int n = 1000;
+std::vector<int> vec = {100, 200 , 300 , 400, 500};
+
+class Solution {
+	private:
+		
+};
+
+int main() {
+	for(int v : vec) {
+		std::cout << vec << std::endl;
+	}
+	return 0;
+}
