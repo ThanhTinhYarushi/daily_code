ed92df1a79c90bcab08ccf0315f87fbe572006be
ThanhTinhYarushi <thanhtinh.yarushi@gmail.com>
2024-04-01 19:55:35 +0700

    done

---
+++ Portfolio-Card-Design/script.js
@@ -0,0 +1,27 @@
+// const allLinks = document.querySelector(".tabs a");
+// const allTabs = document.querySelectorAll(".tab-content");
+
+// allLinks.forEach((link) => {
+//   link.addEventListener("click", () => {
+//     const linkId = link.id;
+
+//     allLinks.forEach((l) => l.classList.toggle("active", l === link));
+//     allTabs.forEach((tab) =>
+//       tab.classList.toggle("active-tab-content", tab.id === linkId)
+//     );
+//   });
+// });
+
+const allLinks = document.querySelectorAll(".tabs a");
+const allTabs = document.querySelectorAll(".tab-content");
+
+allLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    const linkId = link.id;
+
+    allLinks.forEach((l) => l.classList.toggle("active", l === link));
+    allTabs.forEach((tab) =>
+      tab.classList.toggle("active-tab-content", tab.id === linkId)
+    );
+  });
+});
