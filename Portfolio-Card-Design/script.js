377c3570403aaef69f4495986a5ea11526b629e5
ThanhTinhYarushi <thanhtinh.yarushi@gmail.com>
2024-04-02 19:55:06 +0700

    done

--- Portfolio-Card-Design/script.js
+++ Portfolio-Card-Design/script.js
@@ -1,17 +1,3 @@
-// const allLinks = document.querySelector(".tabs a");
-// const allTabs = document.querySelectorAll(".tab-content");
-
-// allLinks.forEach((link) => {
-//   link.addEventListener("click", () => {
-//     const linkId = link.id;
-
-//     allLinks.forEach((l) => l.classList.toggle("active", l === link));
-//     allTabs.forEach((tab) =>
-//       tab.classList.toggle("active-tab-content", tab.id === linkId)
-//     );
-//   });
-// });
-
 const allLinks = document.querySelectorAll(".tabs a");
 const allTabs = document.querySelectorAll(".tab-content");
 
