5b8ce070a6d15fcdcf7b39c82c6c66ec9422c538
ThanhTinhYarushi <thanhtinh.yarushi@gmail.com>
2024-06-10 20:38:33 +0700

    clean

--- HTML/HTML_TESTCODE/script.js
+++ HTML/HTML_TESTCODE/script.js
@@ -1,7 +1,2 @@
 const obj = ["Hello", [1, 2, ["World", "fucking"]]];
 console.table(obj);
-
-for (let index = 0; index < array.length; index++) {
-  const element = array[index];
-  console.log(element);
-}
